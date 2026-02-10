512db34a8543899c2e949a29f13d0446c81972b4
Alexey Dyuryagin <airoko.alex@gmail.com>
2026-02-10 10:23:01 +0500

    Added possibility to like posts

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -12,9 +12,9 @@ public class PostService {
     private List<Post> posts = new ArrayList<>();
 
     public PostService() {
-        posts.add(new Post("Post 1", new Date(2026, 2, 9)));
-        posts.add(new Post("Important Post", new Date(2026, 2, 10)));
-        posts.add(new Post("Very Important Post", new Date(2026, 2, 8)));
+        posts.add(new Post(1L, "Post 1", new Date(2026, 2, 9)));
+        posts.add(new Post(2L, "Important Post", new Date(2026, 2, 10)));
+        posts.add(new Post(3L, "Very Important Post", new Date(2026, 2, 8)));
     }
 
     public List<Post> listAllPosts() {
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public void create(String text) {
-        posts.add(new Post(text, new Date()));
+        posts.add(new Post(Long.valueOf(posts.size() + 1), text, new Date()));
     }
 }
